ddfe9532a83984f1b15801145f7491ff62221037
acchayon <adhikaryac81@gmail.com>
2023-07-22 17:27:17 +0600

    added not found error page

--- src/Pages/Home/Banner/RightContent.jsx
+++ src/Pages/Home/Banner/RightContent.jsx
@@ -1,7 +1,7 @@
 
 const RightContent = () => {
     return (
-        <div>
+        <div className="p-10 md:p-0 lg:p-0">
             <h1 className="text-4xl font-bold mb-5">We will give you bright future</h1>
             <p className=" ">Welcome to Campus Zone. Discover and book your dream college with ease. Browse through our vast selection of colleges and find the perfect one for you. Whether you're looking for a top-ranked university, a specialized program, or a community college, we've got you covered. Start your educational journey with us today and make your college dreams a reality</p>
             
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -3,6 +3,7 @@ import {
   } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
+import ErrorPage from "../Pages/Error/ErrorPage";
 
    export const router = createBrowserRouter([
     {
@@ -15,4 +16,8 @@ import Home from "../Pages/Home/Home/Home";
         }
       ]
     },
+    {
+      path: '*',
+      element: <ErrorPage></ErrorPage>
+    }
   ]);
